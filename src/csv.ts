cbd3364afddea366ebf8d33b38aa93410f37c442
wtotw <s135061@gmail.com>
2024-07-09 22:55:25 +0900

    csv出力

--- src/csv.ts
+++ src/csv.ts
@@ -1,1 +1,54 @@
-import fs from 'fs';
+import * as fs from "node:fs";
+import * as csv from "csv";
+
+import { getUule } from "./uule";
+
+const HEADER = [
+	"会社ID",
+	"会社名",
+	"店舗ID",
+	"店舗名",
+	"stores_latitude",
+	"stores_longitude",
+	"craw_ranking_latitude",
+	"craw_ranking_longitude",
+	"都道府県",
+	"市区町村",
+	"取得地域",
+	"uule",
+	"削除済み",
+] as const;
+
+const input = fs.readFileSync("public/csv/input/list.csv", "utf-8");
+csv.parse(
+	input,
+	{ columns: true },
+	async (_err, data: { [key in (typeof HEADER)[number]]: string }[]) => {
+		console.log(data.length);
+
+		const map = data.map(async (row) => {
+			if (row.削除済み === "TRUE" || !row.取得地域 || row.uule) {
+				return row;
+			}
+
+			const uule = await getUule(row.取得地域);
+			return { ...row, uule: uule };
+		});
+
+		const uuleData = await Promise.all(map);
+
+		console.log(uuleData);
+
+		csv.stringify(
+			uuleData,
+			{ header: true, columns: HEADER },
+			(err, output) => {
+				if (err) {
+					console.error(err);
+					return;
+				}
+				fs.writeFileSync("public/csv/output/list.csv", output);
+			},
+		);
+	},
+);
